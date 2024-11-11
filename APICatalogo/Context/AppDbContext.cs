661dccadd6a4b6cf3c8f40025f67d158bb630f7c
Felipe Rondello <ferondellodev@gmail.com>
2024-11-11 19:41:32 -0300

    Registrando os servicos no container DI

--- APICatalogo/Context/AppDbContext.cs
+++ APICatalogo/Context/AppDbContext.cs
@@ -1,6 +1,16 @@
-﻿namespace APICatalogo.Context
+﻿using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Context
 {
-    public class AppDbContext
+    public class AppDbContext: DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options ) : base(options)
+        {
+            
+        }
+
+        public DbSet<Categoria>? categorias { get; set; }
+        public DbSet<Produto>? Produtos { get; set; }
     }
 }
